9a431befa8e36fc222c8c72c5742ca76d80d5d0b
KamyMantha <kamy_mantha@yahoo.com>
2023-07-18 13:43:54 -0400

    sample producer

---
+++ src/main/java/com/tanzu/kafka/example/Employee.java
@@ -0,0 +1,14 @@
+package com.tanzu.kafka.example;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Employee {
+
+	private Integer id;
+	private String name;
+}
